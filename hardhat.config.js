e2801e2af852ce815832d75d970f90412c03ebfc
Xiangyu Xu <xuxiangy@amazon.com>
2023-11-02 15:38:26 -0700

    init commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_GOERLI_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  },
+};
