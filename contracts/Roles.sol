87815794fbe684889e6535a317432b4c43c6f4e3
Xiangyu Xu <xuxiangy@amazon.com>
2023-11-05 11:52:11 -0800

    polished the Roles lib && finished ConsumerRole contract

--- contracts/Roles.sol
+++ contracts/Roles.sol
@@ -14,8 +14,8 @@ library Roles {
    * @dev give an account access to this role
    */
   function add(Role storage role, address account) internal {
-    require(account != address(0));
-    require(!has(role, account));
+    require(account != address(0), 'You cannot work with a null/burner address!');
+    require(!has(role, account), 'This address already has a role!');
 
     role.bearer[account] = true;
   }
@@ -24,8 +24,8 @@ library Roles {
    * @dev remove an account's access to this role
    */
   function remove(Role storage role, address account) internal {
-    require(account != address(0));
-    require(has(role, account));
+    require(account != address(0), 'You cannot work with a null/burner address!');
+    require(has(role, account), 'This address does not have a role yet!');
 
     role.bearer[account] = false;
   }
@@ -39,7 +39,7 @@ library Roles {
     view
     returns (bool)
   {
-    require(account != address(0));
+    require(account != address(0), 'You cannot work with a null/burner address!');
     return role.bearer[account];
   }
 }
