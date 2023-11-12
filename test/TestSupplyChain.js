872c5371865b63189d93ba7b4565dfa5521dd82c
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-11-12 09:08:03 -0800

    WIP test script - improved the 1st test

--- test/TestSupplyChain.js
+++ test/TestSupplyChain.js
@@ -1,27 +1,32 @@
 // This script is designed to test the solidity smart contract - SuppyChain.sol -- and the various functions within
 
 // const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 const { toUtf8Bytes, keccak256, parseEther } = ethers;
 
 describe('SupplyChain', function(){
+    var sku = 1;
+    var upc = 1;
+    const originFarmName = "John Doe";
+    const originFarmInformation = "Yarray Valley";
+    const originFarmLatitude = "-38.239770";
+    const originFarmLongitude = "144.341490";
+    var productID = sku + upc;
+    const productNotes = "Best beans for Espresso";
+    const productPrice = parseEther('1');
+    var itemState = 0;
+    const emptyAddress = '0x00000000000000000000000000000000000000';
+    var ownerID;
+    var originFarmerID;
+    var distributorID;
+    var retailerID;
+    var consumerID;
+
     it('Contract should deploy just fine', async function() {
         const supplyChain = await ethers.deployContract("SupplyChain");
     
-        const [ownerID, originFarmerID, distributorID, retailerID, consumerID] = await ethers.getSigners();
-        var sku = 1;
-        var upc = 1;
-        const originFarmName = "John Doe";
-        const originFarmInformation = "Yarray Valley";
-        const originFarmLatitude = "-38.239770";
-        const originFarmLongitude = "144.341490";
-        var productID = sku + upc;
-        const productNotes = "Best beans for Espresso";
-        const productPrice = parseEther('1');
-        var itemState = 0;
-        const emptyAddress = '0x00000000000000000000000000000000000000';
-
+        [ownerID, originFarmerID, distributorID, retailerID, consumerID] = await ethers.getSigners();
         console.log("Accounts used here...");
         console.log("Contract Owner: ", ownerID.address);
         console.log("Farmer: ", originFarmerID.address);
