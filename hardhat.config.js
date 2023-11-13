3840f899dbe28f1f22609223eb9feb05df079149
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-11-13 10:16:00 -0800

    Changed some configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
         enabled: true,
         runs: 100,
       },
-      viaIR: true,
+      // viaIR: true,
     },
   },
   
--- scripts/interaction.js
+++ scripts/interaction.js
@@ -1,11 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const supplyChainAddr = '0xfb3939228736a231BE954dc0D887d2DBE415881F';  
+  const supplyChainAddr = '0x4e129C663310777deCBa8e137892e7967C0fD877';  
   const supplyChain = await hre.ethers.getContractAt("SupplyChain", supplyChainAddr);
 
   const owner = await supplyChain.owner()
   console.log(owner)
+
+  await supplyChain.harvestItem(1, "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", "John Doe", "Yarray Valley", "-38.239770", "144.341490", "Best beans for Espresso")
+  const resultBufferOne = await supplyChain.fetchItemBufferOne(1)
+  console.log(resultBufferOne)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
