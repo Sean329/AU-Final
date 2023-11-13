10da99a60190e22527c3a117b12fc48c4a26b5ed
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-11-12 20:22:44 -0800

    WIP test script - 2nd test done writing

--- test/TestSupplyChain.js
+++ test/TestSupplyChain.js
@@ -22,9 +22,10 @@ describe('SupplyChain', function(){
     var distributorID;
     var retailerID;
     var consumerID;
+    var supplyChain;
 
-    it('Contract should deploy just fine', async function() {
-        const supplyChain = await ethers.deployContract("SupplyChain");
+    it('Contract should deploy just fine', async () => {
+        supplyChain = await ethers.deployContract("SupplyChain");
     
         [ownerID, originFarmerID, distributorID, retailerID, consumerID] = await ethers.getSigners();
         console.log("Accounts used here...");
@@ -36,5 +37,28 @@ describe('SupplyChain', function(){
         console.log("SupplyChain contract is at: ", supplyChain.target);
 
         assert.equal(await supplyChain.owner(),ownerID.address);
-    });
+    })
+
+    it("Testing smart contract function harvestItem() that allows a farmer to harvest coffee", async() => {
+        
+        // Mark an item as Harvested by calling function harvestItem()
+        var event = await supplyChain.harvestItem(upc, originFarmerID, originFarmName, originFarmInformation, originFarmLatitude, originFarmLongitude, productNotes)
+        
+        // Determine if the event has been emitted using `expect`
+        expect(event).to.emit(supplyChain, 'Harvested')
+        
+        // Retrieve the just now saved item from blockchain by calling function fetchItem()
+        const resultBufferOne = await supplyChain.fetchItemBufferOne(upc)
+        const resultBufferTwo = await supplyChain.fetchItemBufferTwo(upc)
+        // Verify the result set
+        assert.equal(resultBufferOne[0], sku, 'Error: Invalid item SKU')
+        assert.equal(resultBufferOne[1], upc, 'Error: Invalid item UPC')
+        assert.equal(resultBufferOne[2], originFarmerID.address, 'Error: Missing or Invalid ownerID')
+        assert.equal(resultBufferOne[3], originFarmerID.address, 'Error: Missing or Invalid originFarmerID')
+        assert.equal(resultBufferOne[4], originFarmName, 'Error: Missing or Invalid originFarmName')
+        assert.equal(resultBufferOne[5], originFarmInformation, 'Error: Missing or Invalid originFarmInformation')
+        assert.equal(resultBufferOne[6], originFarmLatitude, 'Error: Missing or Invalid originFarmLatitude')
+        assert.equal(resultBufferOne[7], originFarmLongitude, 'Error: Missing or Invalid originFarmLongitude')
+        assert.equal(resultBufferTwo[5], 0, 'Error: Invalid item State')      
+    })   
 });
